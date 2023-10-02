335c2b11bf1419ec1453593766d502c210683d38
Zelda Mazur <zelda@zeldamazur.com>
2023-10-02 18:35:25 -0500

    scaffolded new components

---
+++ BookIt/src/app/bookshelf/bookshelf.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookshelf',
+  templateUrl: './bookshelf.component.html',
+  styleUrls: ['./bookshelf.component.css']
+})
+export class BookshelfComponent {}
---
+++ BookIt/src/app/library/library.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-library',
+  templateUrl: './library.component.html',
+  styleUrls: ['./library.component.css']
+})
+export class LibraryComponent {}
