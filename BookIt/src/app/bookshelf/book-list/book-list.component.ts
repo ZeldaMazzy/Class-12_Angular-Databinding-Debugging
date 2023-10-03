b3edc4b5e7adcd8f0ef2419685e3be07c93685ea
Zelda Mazur <zelda@zeldamazur.com>
2023-10-02 20:02:36 -0500

    Finished class 12 notes

--- BookIt/src/app/bookshelf/book-list/book-list.component.ts
+++ BookIt/src/app/bookshelf/book-list/book-list.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { TEST_BOOK_LIST } from 'src/app/shared/book/book.constants';
+import { Book } from 'src/app/shared/book/book.model';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class BookListComponent {
+  public bookList: Book[] = TEST_BOOK_LIST.slice()
 }
--- BookIt/src/app/library/book-results/book-results.component.ts
+++ BookIt/src/app/library/book-results/book-results.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { TEST_BOOK_LIST } from 'src/app/shared/book/book.constants';
+import { Book } from 'src/app/shared/book/book.model';
 
 @Component({
   selector: 'app-book-results',
   templateUrl: './book-results.component.html',
   styleUrls: ['./book-results.component.css']
 })
-export class BookResultsComponent implements OnInit {
+export class BookResultsComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  public bookList: Book[] = TEST_BOOK_LIST.slice()
 
 }
