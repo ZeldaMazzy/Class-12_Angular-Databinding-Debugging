df708950cec99e0e1003b4f58286c8b80ffdc069
Zelda Mazur <zelda@zeldamazur.com>
2023-11-02 19:27:35 -0500

    added url to environment variable

--- BookIt/src/environments/environment.prod.ts
+++ BookIt/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseRootURL: "https://bookit-personal-default-rtdb.firebaseio.com/books.json"
 };
--- BookIt/src/environments/environment.ts
+++ BookIt/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseRootURL: "https://bookit-personal-default-rtdb.firebaseio.com/books.json"
 };
 
 /*
