e649381bcd673c43de4299764bfb3ee9c88458a1
ZeldaMazzy <69434725+ZeldaMazzy@users.noreply.github.com>
2023-12-15 17:04:24 -0600

    Update app.component.ts

--- BookIt/src/app/app.component.ts
+++ BookIt/src/app/app.component.ts
@@ -10,11 +10,5 @@ export class AppComponent {
   title = 'BookIt';
   currentPage: string = "bookshelf";
 
-  constructor(private auth: AuthService) {
-    // if(!auth.getUserDataFromStorage()) return;
-    // const data: AuthResponse = auth.getUserDataFromStorage();
-    // if(auth.getAccessTokenFromStorage().length > 0) {
-    //   auth.handleAuth(data)
-    // }
-  }
+  constructor(private auth: AuthService) {}
 }
